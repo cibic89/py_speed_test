ce74952713c296195521a66ac93b02a6498d11a9
George Joseph <gjnet@live.com>
2020-01-28 14:52:19 +0000

    removed GUI and kept console instead

--- src/PySpeedTestGUI.py
+++ src/PySpeedTestGUI.py
@@ -3,23 +3,22 @@
 
 import gc
 import os
+import re
 import sys
+import time
 
-import PySimpleGUI as sg
-from pandas import DataFrame, to_datetime, concat  # , read_csv
 import speedtest
+from pandas import DataFrame, to_datetime, concat  # , read_csv
 
 bit_to_mb_factor = 10 ** 6
 
-main_dir = str(os.path.normpath(os.getcwd()+os.sep+os.pardir))
+main_dir = str(os.path.normpath(os.getcwd()+os.sep+os.pardir+os.sep+os.pardir))
 images_dir = main_dir+os.sep+"images"+os.sep+"exports"+os.sep
 output_dir = main_dir+os.sep+"data"+os.sep+"processed"+os.sep
-print(main_dir+"\n"+images_dir+"\n"+output_dir)
+# print(main_dir+"\n"+images_dir+"\n"+output_dir)
 
 if not os.path.isdir(output_dir):
     os.mkdir(output_dir)
-
-app_logo_fname = "app_logo.ico"
 
 results = dict()
 results_df = DataFrame()
@@ -28,17 +27,45 @@ cities_rgx = "United Kingdom|Italy|Russian Federation|Estonia|Albania|Tunisia"
 cities_rgx += "Morocco|Turkey|United States"
 cities_rgx += "United Arab Emirates|China|India|Uganda|Brazil|Mexico"
 
-test_speed = sg.PopupYesNo("Test your worldwide internet speed?", title="WST",
-                           icon=images_dir+os.sep+app_logo_fname)
+yes = {'yes', 'y', 'ye', ''}
+no = {'no', 'n'}
+test_speed = None
+save_results = None
+usable_input = None
+
+
+def filename_check(strg, search=re.compile(r'[\"\'\\/]').search):
+    return bool(search(strg))
+
+
+def yn_input(question):
+    choice = input(question + " (y/n) [y]: ").lower().strip()
+    if choice in yes:
+        return True
+    elif choice in no:
+        return False
+    else:
+        print("Please respond with 'y' for yes or 'n' for n ")
+        return None
 
 
 def close_all():
-    """Closes debug outputs and exits program"""
-    sg.EasyPrintClose()
+    print("\nThank you for using Worldwide Speedtest® by Dr George Joseph 2020")
+    time.sleep(5)
     sys.exit()
 
 
-if test_speed == "Yes":
+print("\nWelcome to Worldwide Speed Test. \n\nFor this to work, please close" +
+      " all internet related applications from all devices using the test" +
+      " network. Optionally restart the broadband device, set your" +
+      " test machine's power profile to high performance, and use the wired network.\n")
+
+
+while test_speed is None:
+    test_speed = yn_input("Test your worldwide internet speed?")
+
+
+if test_speed:
     s = speedtest.Speedtest()
     servers_df = DataFrame.from_dict(s.get_servers(), orient="index")
     servers_df = servers_df[0].reset_index(drop=True)
@@ -51,14 +78,10 @@ if test_speed == "Yes":
     server_inds = server_inds.drop_duplicates().index
 
     start_time = to_datetime('now')
-    Print = sg.EasyPrint  # (do_not_reroute_stdout=False)
-    Print("Welcome to Worldwide Speed Test. \n\nFor this to work, please close" +
-          " all internet related applications from all devices using the test" +
-          " network. Optionally restart the broadband device and set your" +
-          " power profile to high performance." +
-          "\n\nINFO: start time - "+start_time.strftime(timestamp_fmt)+" - currently speed testing:")
+    # print = sg.Easyprint  # (do_not_reroute_stdout=False)
+    print("\n\nINFO: start time - "+start_time.strftime(timestamp_fmt)+" - currently speed testing:")
 
-    for i, server in enumerate(server_inds):
+    for i, server in enumerate(server_inds[:1]):
         s = speedtest.Speedtest()
 
         servers = [server]  # has to be one only
@@ -78,7 +101,7 @@ if test_speed == "Yes":
         results[server] = s.results.dict()
 
         #         clear_output(wait=True)
-        Print(servers_df["name"].loc[server]+", "+servers_df["country"].loc[server])
+        print(servers_df["name"].loc[server]+", "+servers_df["country"].loc[server])
 
     results_df = DataFrame(results).T
 
@@ -97,27 +120,35 @@ if test_speed == "Yes":
     gc.collect()
 
     finish_time = to_datetime('now')
-    Print("INFO: end time - ", str(finish_time.strftime(timestamp_fmt))+". This took",
+    print("INFO: end time - ", str(finish_time.strftime(timestamp_fmt))+". This took",
           round((finish_time - start_time).total_seconds() / 60, 2), "minutes")
 
     # results_df = read_csv(output_dir+isp_name_package+".csv")
-    Print("\n\nMedian download speed is",
-          round(results_df["download"].median() / bit_to_mb_factor, 2), "mb",
+    print("\n\nMedian download speed is", round(results_df["download"].median() / bit_to_mb_factor, 2), "mb",
           "\nMedian upload speed is", round(results_df["upload"].median() / bit_to_mb_factor, 2), "mb",
-          "\nPing is", round(results_df["ping"].median(), 2), "ms")
+          "\nPing is", round(results_df["ping"].median(), 2), "ms\n")
 else:
     close_all()
 
 # display(results_df.head())
 
-save_results = sg.PopupYesNo("Save results as a csv file?", title="WST", icon=images_dir+os.sep+app_logo_fname)
 
-if (test_speed == "Yes") & (save_results == "Yes"):
-    text_in = sg.PopupGetText('Please enter filename (without extension)',
-                              title="WST",
-                              icon=images_dir+os.sep+app_logo_fname)
-    if text_in is not None:
-        results_df.to_csv(output_dir+text_in+".csv", index=False)
-    close_all()
+while save_results is None:
+    save_results = yn_input("Save results as a csv file?")
+
+
+if test_speed & save_results:
+    text_in = input('Please enter filename (without extension) [wst]: ') or "wst"
+
+    while not usable_input:
+        if (text_in is None) | (text_in == "") | (filename_check(text_in)):
+            usable_input = False
+            print("Please input a usable filename...")
+        else:
+            full_output_fname = output_dir + text_in + ".csv"
+            usable_input = True
+            results_df.to_csv(full_output_fname, index=False)
+            print("Results exported as:\n"+full_output_fname)
+            close_all()
 else:
     close_all()
